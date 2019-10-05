359fc1b8a9d726b9db2581aa65d0096ecbf86209
samantha <sdesmul@mail.greenriver.edu>
2019-10-04 18:12:42 -0700

    Initial commit for fat free setup.

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samantha
+ * Date: 2019-10-04
+ * Time: 15:09
+ */
+
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once('vendor/autoload.php');
+
+//create an instance of the Base class/ fat free object
+$f3 = Base::instance();
+
+//Define a default root, there can be multiple routes
+
+//homepage
+$f3->route('GET /', function () {
+    //display the contents of the page
+    $view = new Template();
+    echo $view->render('views/header.html');
+    echo $view->render("views/home_page.html");
+    echo $view->render('views/footer.html');
+});
+//run Fat-free
+$f3->run();
