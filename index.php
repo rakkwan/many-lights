6b0b0abfb4744279e31170000d584aa3974ccca2
samantha <sdesmul@mail.greenriver.edu>
2019-10-10 23:20:15 -0700

    Added datatable CDNs to header html and footer. Added datatable.js. Added datatable html with dummmy values

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ error_reporting(E_ALL);
 
 //Require autoload file
 require_once('vendor/autoload.php');
-require_once ('model/validation.php');
+require_once('model/validation.php');
 
 //create an instance of the Base class/ fat free object
 $f3 = Base::instance();
@@ -39,24 +39,22 @@ $f3->route('GET /home', function () {
     echo $view->render('views/includes/footer.html');
 });
 
-$f3->route('GET /resources', function () {
-      //Test
-//    echo "<h1>This is TEST resources Page</h1>";
-//        print_r($_POST);
+$f3->route('GET /resources', function ($f3) {
+
     //display the contents of the page
     $view = new Template();
-    echo $view->render('views/includes/header.html');
+    $f3->set('title', "Resources");
+
+//    echo $view->render('views/includes/header.html');
     echo $view->render("views/resources.html");
-    echo $view->render('views/includes/footer.html');
+//    echo $view->render('views/includes/footer.html');
 });
 
 // recommended form
-$f3->route('GET|POST /recommended', function ($f3)
-{
+$f3->route('GET|POST /recommended', function ($f3) {
 
-     //If form has been submitted, validate
-    if(!empty($_POST))
-    {
+    //If form has been submitted, validate
+    if (!empty($_POST)) {
         // Get data from form
         $fname = $_POST['fname'];
         $lname = $_POST['lname'];
@@ -72,8 +70,7 @@ $f3->route('GET|POST /recommended', function ($f3)
         $f3->set('message', $message);
 
         // if data is valid
-        if (validForm())
-        {
+        if (validForm()) {
             // Write data to session
             $_SESSION['fname'] = $fname;
             $_SESSION['lname'] = $lname;
@@ -92,8 +89,7 @@ $f3->route('GET|POST /recommended', function ($f3)
     echo $view->render('views/includes/footer.html');
 });
 
-$f3->route('GET|POST /confirmation', function ($f3)
-{
+$f3->route('GET|POST /confirmation', function ($f3) {
     //display the confirmation of the page
     $view = new Template();
     echo $view->render('views/includes/header.html');
