c6a07c1d2185fa6ea7f964349bdf1f0b3c479bc3
rakkwan <jgoodrich4@greenriver.edu>
2019-10-16 21:59:05 -0400

    :zap::green_heart: Committed to Repo to catch server up with Master branch. Misc Changes @all
    Sent notification to group about the Server Pull from repo update but no one knew of issue Updated Server to current master branch

---
+++ views/javascript/datatable.js
@@ -0,0 +1,101 @@
+$(document).ready(function () {
+    $('#resources').DataTable({
+        "data": [
+            [
+                "Therapist",
+                "Kristie Baber, MSW, LICSW",
+                "LodeStar Therapy",
+                "(206)673-3730",
+                "N/A",
+                "4500 9Tth Ave Ne Suite 300" +
+                "Seattle, WA 98105"
+            ],
+            [
+                "Therapist",
+                "Amy Baker D' Alessandro",
+                "N/A",
+                "(206)661-2466",
+                "abaker@nwfamilylife.org",
+                "10550 Lake City Way NE suit E " +
+                "Seattle, WA 98125"
+            ],
+            [
+                "Therapist",
+                "Jessie Brooks",
+                "Janzen, MSW",
+                "(206)905-9931",
+                "N/A",
+                "5100 S Dawson St " +
+                "Suit #104 Seattle, WA 98118"
+            ],
+            [
+                "Therapist",
+                "Megan Clarke, MA, LMFT",
+                "N/A",
+                "N/A",
+                "N/A",
+                "1800 112th Avenue NE 240W Bellevue, WA 98004"
+            ],
+            [
+                "Therapist",
+                "Amy Dutt, LMFT",
+                "N/A",
+                "(509)378-7753",
+                "N/A",
+                "7409 W. Grandridge Blvd, Ste " +
+                "101 Kennewick, WA 99336"
+            ],
+            [
+                "Crisis",
+                "Child Protective Services",
+                "N/A",
+                "1-866-363-4276",
+                "N/A",
+                "N/A"
+            ],
+            [
+                "Food Assistance",
+                "Enumclaw Kiwanis Food Bank",
+                "N/A",
+                "360-825-6188 ",
+                "N/A",
+                "N/A"
+            ],
+            [
+                "Food Assistance",
+                "Childrens Home Society Of Washington",
+                "N/A",
+                "206-364-7930",
+                "N/A",
+                "North Seattle Family Center"
+            ],
+            [
+                "Food Assistance",
+                "Highline Area Food Bank",
+                "N/A",
+                "206-433-9900 ",
+                "N/A",
+                "N/A"
+            ],
+            [
+                "Therapist",
+                "Lindsay P. Dye, MA, LMHC, NCC",
+                "N/A",
+                "(509)378-7753",
+                "N/A",
+                "7409 W. Grandridge Blvd, Ste 101 " +
+                "Kennewick, WA 99336"
+            ],
+            [
+                "Therapist",
+                "Jill Dziko",
+                "N/A",
+                "(206)542-7516",
+                "N/A",
+                "18532 Firlands Way N " +
+                "Shoreline, WA 98133"
+            ]
+        ]
+    });
+    console.log("JS loaded");
+});
