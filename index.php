a75f76656e9bf19884039f60571a655d4a80bc0e
Sang Le <lesan6021@gmail.com>
2019-10-28 18:53:31 -0400

    Add DayHour class

--- index.php
+++ index.php
@@ -304,11 +304,10 @@ $f3->route('GET|POST /dayHour', function ($f3) {
     if (!empty($_POST)) {
         // Get data from form
         $days = $_POST['days'];
-        $_SESSION['days'] = $days;
 
         if (!empty($days)) {
             // loop through and check which days are checked
-            foreach ($_SESSION['days'] as $day) {
+            foreach ($_POST['days'] as $day) {
                 if (isset($day)) {
                     // Add data to hive and set session for time
                     $_SESSION[$day . 'FromTime'] = date('h:i A', strtotime($_POST[$day . 'FromTime']));
@@ -320,18 +319,27 @@ $f3->route('GET|POST /dayHour', function ($f3) {
             }
         }
 
+        if(sizeof($days) == 0) {
+            $days = array('No days selection');
+        }
+
+
         $countyOne = $_POST['countyOne'];
         $countyTwo = $_POST['countyTwo'];
         $countyThree = $_POST['countyThree'];
 
         // Add data to hive
+        $f3->set('day', $days);
         $f3->set('countyOne', $countyOne);
         $f3->set('countyTwo', $countyTwo);
         $f3->set('countyThree', $countyThree);
 
+        $_SESSION['DayHourInfo'] = new DayHourInfo($days, $countyOne, $countyTwo, $countyThree);
+
         // if data is valid
         if (!empty($_POST)) {
             // Write data to session
+            $_SESSION['days'] = $days;
             $_SESSION['countyOne'] = $countyOne;
             $_SESSION['countyTwo'] = $countyTwo;
             $_SESSION['countyThree'] = $countyThree;
@@ -341,6 +349,10 @@ $f3->route('GET|POST /dayHour', function ($f3) {
         }
     }
 
+    if(!isset($_SESSION['DayHourInfo'])) {
+        $noInput = array('1', '2');
+        $_SESSION['DayHourInfo'] = new DayHourInfo($noInput, '', '', '');
+    }
 
     //display the day and hour of the page
     $view = new Template();
